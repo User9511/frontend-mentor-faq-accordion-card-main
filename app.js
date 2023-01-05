30ecbcddef6bae6ce498640972639d6004842100
Smgy94 <Shanemcgeown@gmail.com>
2023-01-05 16:17:14 +0000

    commit

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 document.querySelectorAll(".accordion__question").forEach((item) => {
   item.addEventListener("click", (event) => {
-    console.log("click!");
-    item.classList.toggle("open");
-    item.nextElementSibling.classList.toggle("open");
+    // If the clicked element is already open, close it. Otherwise, open it.
+    if (item.classList.contains("open")) {
+      item.classList.remove("open");
+      item.nextElementSibling.classList.remove("open");
+    } else {
+      // Close all accordion items
+      document.querySelectorAll(".accordion__question").forEach((item) => {
+        item.classList.remove("open");
+        item.nextElementSibling.classList.remove("open");
+      });
+
+      item.classList.add("open");
+      item.nextElementSibling.classList.add("open");
+    }
   });
 });
